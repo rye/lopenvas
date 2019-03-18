3462101036ca7bd11624969a09bba5b3dda1e87f
Kristofer Rye <kristofer.rye@gmail.com>
2019-03-18 14:36:40 -0500

    pre_warm: Set up functions needed to download files
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- pre_warm.sh
+++ pre_warm.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 set -x
+
+ROOT="$(dirname $0)/var"
+
+mkdir -pv "$ROOT"
+
+fetch_intermediate() {
+	if [ -z "$2" ];
+	then
+		wget -N -P "$ROOT" "$1"
+	else
+		wget -O "$2" "$1"
+	fi
+}
+
+pushd "$ROOT"
+
+sha256sum -c SHA256SUMS
