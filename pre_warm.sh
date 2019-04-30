1864b23df292b0e6882ebf896c0562e52a0a6db0
Kristofer Rye <kristofer.rye@gmail.com>
2019-04-29 19:30:13 -0500

    Remove now-unused source archives
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- pre_warm.sh
+++ pre_warm.sh
@@ -15,25 +15,10 @@ fetch_intermediate() {
 	fi
 }
 
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.13.tar.bz2"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/gpgme/gpgme-1.12.0.tar.bz2"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.3.tar.bz2"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.35.tar.gz"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.4.tar.gz"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/libksba/libksba-1.3.5.tar.bz2"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2"
-fetch_intermediate "https://gnupg.org/ftp/gcrypt/ntbtls/ntbtls-0.1.2.tar.bz2"
-
-fetch_intermediate "ftp://xmlsoft.org/libxslt/libxslt-1.1.33.tar.gz"
-
-fetch_intermediate "https://github.com/Kitware/CMake/releases/download/v3.14.0/cmake-3.14.0.tar.gz"
 fetch_intermediate "https://github.com/greenbone/gsa/archive/v8.0.0.tar.gz" "$ROOT/gsa-8.0.0.tar.gz"
 fetch_intermediate "https://github.com/greenbone/gvm-libs/archive/v10.0.0.tar.gz" "$ROOT/gvm-libs--10.0.0.tar.gz"
-#fetch_intermediate "https://github.com/greenbone/gvm-libs/archive/v9.0.3.tar.gz" "$ROOT/gvm-libs--9.0.3.tar.gz"
-#fetch_intermediate "https://github.com/greenbone/gvmd/archive/v7.0.3.tar.gz" "$ROOT/gvmd-7.0.3.tar.gz"
 fetch_intermediate "https://github.com/greenbone/gvmd/archive/v8.0.0.tar.gz" "$ROOT/gvmd-8.0.0.tar.gz"
 fetch_intermediate "https://github.com/greenbone/openvas-scanner/archive/v6.0.0.tar.gz" "$ROOT/openvas-scanner--6.0.0.tar.gz"
-#fetch_intermediate "https://github.com/greenbone/openvas-scanner/archive/v5.1.3.tar.gz" "$ROOT/openvas-scanner--5.1.3.tar.gz"
 fetch_intermediate "https://github.com/greenbone/openvas-smb/archive/v1.0.4.tar.gz" "$ROOT/openvas-smb--1.0.4.tar.gz"
 
 pushd "$ROOT"
