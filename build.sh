f239681f1646a1cb02d927319c3eefe349c154e2
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-08 08:23:34 -0500

    Introduce a dependency chain and make sure that all builds are successful
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ function build_target_to_tag() {
 
 VERSION="${VERSION:-latest}"
 
-build_target_to_tag "base" "openvas-docker/base:${VERSION}"
-build_target_to_tag "sync" "openvas-docker/sync:${VERSION}"
-build_target_to_tag "openvas" "openvas-docker/openvas:${VERSION}"
-build_target_to_tag "gvmd" "openvas-docker/gvmd:${VERSION}"
+build_target_to_tag "base" "openvas-docker/base:${VERSION}" && \
+build_target_to_tag "sync" "openvas-docker/sync:${VERSION}" && \
+build_target_to_tag "openvas" "openvas-docker/openvas:${VERSION}" && \
+build_target_to_tag "gvmd" "openvas-docker/gvmd:${VERSION}" && \
 build_target_to_tag "gsad" "openvas-docker/gsad:${VERSION}"
