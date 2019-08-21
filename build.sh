89578653b25d4c2a67f079452c04d7fc9bf0afc1
Kristofer Rye <kristofer.rye@gmail.com>
2019-08-21 10:45:45 -0500

    Also build to docker.pkg.github.com
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>

--- build.sh
+++ build.sh
@@ -10,9 +10,15 @@ function retag() {
 
 VERSION="${VERSION:-latest}"
 
-build_target_to_tag "base" "lopenvas/base:${VERSION}" && \
-build_target_to_tag "sync" "lopenvas/sync:${VERSION}" && \
-build_target_to_tag "openvas" "lopenvas/openvas:${VERSION}" && \
-build_target_to_tag "gvmd" "lopenvas/gvmd:${VERSION}" && \
-build_target_to_tag "gvmd-db" "lopenvas/postgres:${VERSION}" && \
-build_target_to_tag "gsad" "lopenvas/gsad:${VERSION}"
+build_target_to_tag "base" "docker.io/lopenvas/base:${VERSION}" && \
+	retag "docker.io/lopenvas/base:${VERSION}" "docker.pkg.github.com/rye/lopenvas/base:${VERSION}" && \
+build_target_to_tag "sync" "docker.io/lopenvas/sync:${VERSION}" && \
+	retag "docker.io/lopenvas/sync:${VERSION}" "docker.pkg.github.com/rye/lopenvas/sync:${VERSION}" && \
+build_target_to_tag "openvas" "docker.io/lopenvas/openvas:${VERSION}" && \
+	retag "docker.io/lopenvas/openvas:${VERSION}" "docker.pkg.github.com/rye/lopenvas/openvas:${VERSION}" && \
+build_target_to_tag "gvmd" "docker.io/lopenvas/gvmd:${VERSION}" && \
+	retag "docker.io/lopenvas/gvmd:${VERSION}" "docker.pkg.github.com/rye/lopenvas/gvmd:${VERSION}" && \
+build_target_to_tag "gvmd-db" "docker.io/lopenvas/postgres:${VERSION}" && \
+	retag "docker.io/lopenvas/postgres:${VERSION}" "docker.pkg.github.com/rye/lopenvas/postgres:${VERSION}" && \
+build_target_to_tag "gsad" "docker.io/lopenvas/gsad:${VERSION}" && \
+	retag "docker.io/lopenvas/gsad:${VERSION}" "docker.pkg.github.com/rye/lopenvas/gsad:${VERSION}"
